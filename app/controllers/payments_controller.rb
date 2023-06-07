416a30cf86b49b76b9c17d90962034e37ef53cc6
ABHISHEK-DEY-64150 <abhishekdey538@gmail.com>
2023-06-07 18:46:28 +0600

    some logics updated on payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 class PaymentsController < ApplicationController
     def payment
         @unpaidpack = CustomerSubscription.find(params[:id]);
+        # @pay = Payment.new 
     end
 
     def paymenthistory
@@ -37,17 +38,17 @@ class PaymentsController < ApplicationController
               hash = Digest::SHA256.hexdigest(combined)[0,10]
 
               @pay.txid = hash 
-
-              
       
+
               if @pay.save 
                 duePackages = CustomerSubscription.where(id: params[:id])
                 duePackages.update(dues:0)
-                redirect_to "/paymenthistory",notice: 'Payment done'
+                redirect_to "/paymenthistory",notice: 'Payment done' 
 
               else
-                #  flash[:payment_errors] = "payment error"
-                 render :payment,@pay => @pay 
+                #   error_message = @pay.errors.full_messages
+                #   flash[:payment_errors] = error_message
+                  render:payment,@pay=>@pay
               end 
     end
 
