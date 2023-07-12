67d1a6e5db7fd5e78f10084d522a261b7083b3fe
rajoy99 <rafat.joy99@gmail.com>
2023-07-12 17:19:36 +0600

    multiple reply functionality

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -129,12 +129,19 @@ class ProvidersController < ApplicationController
 
     creview = Review.find_by_id(params[:id])
 
-    creview.reply=params[:reply]
+    p "TYPE", creview.reply.class
 
+    if creview.reply
+      old=creview.reply+creview.created_at.to_s
+      newr=params[:reply]
+      creview.reply="#{old}\n#{newr}"
+    else
+      creview.reply=params[:reply]
+    end
     p "Check Updated Review", creview
 
     if creview.save
-      redirect_to '/providerDashboard', notice: "Reply updated successfully."
+      redirect_to '/showreviews', notice: "Reply updated successfully."
     else
       render '/dues'
     end
